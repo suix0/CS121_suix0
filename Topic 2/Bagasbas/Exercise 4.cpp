fee2a67346c8db1da19bca1ab55aa8627d874177
suix0 <bagasbas.nethangabriel@gmail.com>
2023-03-03 22:40:00 +0800

    made changes to exercise 4 topic 2

---
+++ Topic 2/Bagasbas/Exercise 4.cpp
@@ -0,0 +1,21 @@
+/* A program that prompts the user to enter the driving
+distance to drive, fuel efficieny of the car in miles per galon,
+and the price per galon, and displays the cost of the trip*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Prompt user to enter driving distance
+    double drivingDistance = 0.0;
+    cout << "Enter the driving distance: "; cin >> drivingDistance;
+    
+    // Prompt user to enter miles per galon
+    double milesPerGal = 0.0;
+    cout << "Enter miles per galon: "; cin >> milesPerGal;
+
+    // Prompt user to enter price per galon
+    double pricePerGal = 0.0;
+    cout << "Enter price per galon: "; cin >> pricePerGal;
+
+}
