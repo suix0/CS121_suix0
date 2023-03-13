c4dd039cc90507fa0f2fa33d9882e74ed122cb03
suix0 <bagasbas.nethangabriel@gmail.com>
2023-03-13 10:51:32 +0800

    added comments in t4e4

--- Topic 4/Bagasbas/Exercise 4.cpp
+++ Topic 4/Bagasbas/Exercise 4.cpp
@@ -4,9 +4,13 @@ check whether the letter is vowel or consonant */
 #include <iostream>
 #include <string>
 
+// Define a function that will loop through a string of vowels and check if
+// the input letter is vowel or consonant
 char vowelOrCons(char c){
     std::string vowel = "AaEeIiOoUu";
+    // The counter will be what we will use to check each index of the string
     int counter = 0;
+    // Loop through each string in the vowel variable and check if the letter is vowel or consonant
     for (int i = 0; i <= vowel.size(); i++){
         if (c == vowel[counter]){
             std::cout << c << " is a vowel" << std::endl;
